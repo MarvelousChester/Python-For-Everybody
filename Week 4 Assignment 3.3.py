cdc6c577a7987cf7ab11cca5a5fe499a090b8157
MarvelousChester <90730377+MarvelousChester@users.noreply.github.com>
2021-09-14 18:54:03 -0400

    Create Week 4 Assignment 3.3.py

---
+++ Week 4 Assignment 3.3.py
@@ -0,0 +1,23 @@
+# Write a program to prompt for a score between 0.0 and 1.0. IF the score out of range, print an error.
+# if the score is between 0.0 and 1.0, print a grade using the following table
+"""
+scr = float(input("Enter a score between 0.0 and 1.0 "))
+
+if scr >= 1:
+    print("Score out of range, Please enter valid score")
+elif scr >= 0.9:
+    print("A")
+elif scr >= 0.8:
+    print("B")
+elif scr >= 0.7:
+    print("C")
+elif scr >= 0.6:
+    print("D")
+elif scr < 0.6 < 0:
+    print("F")
+else:
+    print("Score out of range, Please enter valid score")
+
+big = max("Hello World")
+print(big)
+small = min("Hello World")
