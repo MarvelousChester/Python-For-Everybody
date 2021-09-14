9c3f93d81a5df36beac9258c2d2acccf380279de
MarvelousChester <90730377+MarvelousChester@users.noreply.github.com>
2021-09-14 18:42:37 -0400

    Create Week 3, Assignment 2.2.py

---
+++ Week 3, Assignment 2.2.py
@@ -0,0 +1,15 @@
+"""""
+#figuring out what order of how the digits will do
+x = 1 + 2 * 3 - 8 / 4
+print(x)
+
+# seeing if it will round up or down and its seems to round down
+x = int(98.6)
+print(x)
+"""
+# Assignment 2.2 Coursera
+# Asks for the users name and welcomes them.
+print("What is your name? ")
+UserName = input()
+print("Welcome", UserName)
+
