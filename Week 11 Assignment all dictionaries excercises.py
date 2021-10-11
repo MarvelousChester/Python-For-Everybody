5aeeb05bf651d792a2e876484f6c46f96b15129d
MarvelousChester <90730377+MarvelousChester@users.noreply.github.com>
2021-10-11 12:14:38 -0400

    Update Week 11 Assignment all dictionaries excercises.py

--- Week 11 Assignment all dictionaries excercises.py
+++ Week 11 Assignment all dictionaries excercises.py
@@ -2,8 +2,7 @@
 # To do this look for lines that start with “From”, then look for the third word and keep a running count of each of the
 # days of the week. At the end of the program print out the contents of your dictionary (order does not matter). mbox
 
-# For tomorrow Do this again but try first get the amount of times the emails repeat and how many times
-# Checks how many times each email is repeated / own exercise for dict()
+# Checks dates of email
 dates = dict()
 text = open("mbox.txt")
 for line in text:
@@ -40,3 +39,48 @@ for line in text:
         else:
             rep[i] += 1
 print(dates)
+
+# Exercise 4: Add code to the above program to figure out who has the most messages in the file. After all the data
+# has been read and the dictionary has been created, look through the dictionary using a maximum loop (see Chapter 5:
+# Maximum and minimum loops) to find who has the most messages and print how many messages the person has.
+
+
+rep = dict()
+text = open("mbox.txt")
+for line in text:
+    if not line.startswith("From:"):  # for the actual code change it From
+        continue
+    line = line.rstrip()
+    word = line.split()
+    del word[0:1]  # deletes From from being printed it out and from the list
+    for i in word:
+        if i not in rep:
+            rep[i] = 1
+        else:
+            rep[i] += 1
+print(rep)
+
+# loop method
+largest = None
+largest_key = None
+smallest = None
+smallest_key = None
+
+# Finding the largest
+for i in rep:
+    if largest is None or rep[i] > largest:
+        largest = rep[i]
+        largest_key = i
+# Finding The smallest
+for i in rep:
+    if smallest is None or rep[i] < smallest:
+        smallest = rep[i]
+        smallest_key = i
+
+print(largest_key, largest, smallest_key, smallest)
+# Easy way to do it
+highest = max(rep, key=rep.get,)   # Highest Value name
+highest_V = max(rep.values())  # Highest Value int
+
+# print(highest, highest_V)
+
